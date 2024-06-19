e407bc2518c0a0f21339ae290ef15e42c495f5ee
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-06-19 16:57:25 +0300

    refactor: Change JourFerie attributes

--- src/main/java/model/JourFerie.java
+++ src/main/java/model/JourFerie.java
@@ -1,15 +1,14 @@
 package model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class JourFerie {
     private LocalDate date;
-    private String description;
+
+    public JourFerie(LocalDate date) {
+        this.date = date;
+    }
 }
