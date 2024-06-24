5c3bdb36fc8e4e752a51dc39ad7322141d84505f
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-06-24 10:37:53 +0300

    chore: Change Calendrier logic for tests

--- src/main/java/model/Calendrier.java
+++ src/main/java/model/Calendrier.java
@@ -1,24 +1,30 @@
 package model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Calendrier {
     private List<LocalDate> joursTravailles;
     private List<JourFerie> joursFeries;
 
-    public boolean estJourFerie(LocalDate date) {
-        return joursFeries.stream().anyMatch(jf -> jf.getDate().equals(date));
+    public boolean estJourTravaille(LocalDate date) {
+        for (JourFerie jourFerie : joursFeries) {
+            if (jourFerie.getDate().equals(date)) {
+                return false;
+            }
+        }
+        return joursTravailles.contains(date);
     }
 
-    public boolean estJourTravaille(LocalDate date) {
-        return joursTravailles.contains(date);
+    public boolean estJourFerie(LocalDate date) {
+        for (JourFerie jourFerie : joursFeries) {
+            if (jourFerie.getDate().equals(date)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
